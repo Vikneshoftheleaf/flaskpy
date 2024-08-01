bee4bd6ffdd5148708b3e240e3da5ccb9964270b
Vikneshoftheleaf <114404122+Vikneshoftheleaf@users.noreply.github.com>
2024-08-01 10:19:25 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,27 @@
-#hrello
+
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return render_template('index.html', id=2)
+
+@app.route('/hello/<slug>')
+
+def helloMsg(slug):
+    return "hello %s" %slug
+
+
+@app.route('/error')
+def err():
+    return abort(404)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3001)
